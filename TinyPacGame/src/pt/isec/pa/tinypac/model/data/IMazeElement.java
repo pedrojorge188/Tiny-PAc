c816c2b9f66d181d2e700c0dae1a652bc5489dc0
Pedro Jorge <pedrorodriguesjorge03@gmail.com>
2023-04-02 22:27:28 +0100

    import teacher code

---
+++ TinyPacGame/src/pt/isec/pa/tinypac/model/data/IMazeElement.java
@@ -0,0 +1,6 @@
+package pt.isec.pa.tinypac.model.data;
+public interface IMazeElement {
+    char getSymbol(); // returns the symbol of this element
+    // The list of symbols is available
+    // in the description of this work
+}
